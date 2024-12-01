7e5e8649f9d7db17e73ba32ae61d77292fb10bda
ihabfaour <iaf08@mail.aub.edu>
2024-12-01 02:33:47 +0200

    created containers for customers and inventory services, and regenrated documentation

--- database/db_config.py
+++ database/db_config.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 # Example Config
 class Config:
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ihab2003*@mysql-container:3306/ecommerce_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ihab2003*@mysql-container:3306/ecommerce_db"
     #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ihab2003*@localhost:3306/ecommerce_db"
-    SQLALCHEMY_DATABASE_URI ='mysql+pymysql://root:N4j1n4j1@localhost:3306/ecommerce_db'
+    #SQLALCHEMY_DATABASE_URI ='mysql+pymysql://root:N4j1n4j1@localhost:3306/ecommerce_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
